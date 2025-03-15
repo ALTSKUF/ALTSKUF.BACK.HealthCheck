f271ba51c5df94f1a48e55bdee0ad49841cfe851
Vladimir <vladimirgjob@gmail.com>
2025-03-16 02:55:07 +0300

    Test kafka

--- Program.cs
+++ Program.cs
@@ -1,11 +1,35 @@
+﻿using Confluent.Kafka;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Добавляем Kafka Consumer для получения запросов
+builder.Services.AddSingleton<IConsumer<Null, string>>(sp =>
+{
+    var config = new ConsumerConfig
+    {
+        BootstrapServers = "localhost:9092",
+        GroupId = "request-group",
+        AutoOffsetReset = AutoOffsetReset.Earliest
+    };
+    return new ConsumerBuilder<Null, string>(config).Build();
+});
+
+// Добавляем Kafka Producer для отправки ответов
+builder.Services.AddSingleton<IProducer<Null, string>>(sp =>
+{
+    var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+    return new ProducerBuilder<Null, string>(config).Build();
+});
+
+// Добавляем фоновую службу для обработки запросов и отправки ответов
+builder.Services.AddHostedService<KafkaRequestHandlerService>();
+
+// Добавляем HealthChecks
 builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
 app.UseHealthChecks("/health");
 
-
 app.Run();
-
